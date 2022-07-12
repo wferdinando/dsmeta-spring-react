5d2edea77b4b27137d632d3b901fde9704bb3f81
Willyan Ferdinando <willyantferdinando@gmail.com>
2022-07-12 20:45:35 -0300

    Project Created

---
+++ backend/src/test/java/io/github/wferdinando/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wferdinando.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

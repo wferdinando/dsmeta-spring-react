a9aac081f5636e521270eea31e00e22c42534890
Willyan Ferdinando <willyantferdinando@gmail.com>
2022-07-14 20:52:03 -0300

    Date select

--- backend/src/main/java/io/github/wferdinando/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/io/github/wferdinando/dsmeta/controllers/SaleController.java
@@ -1,9 +1,10 @@
 package io.github.wferdinando.dsmeta.controllers;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.wferdinando.dsmeta.entities.Sale;
@@ -14,14 +15,17 @@ import io.github.wferdinando.dsmeta.services.SaleService;
 public class SaleController {
 
 	private final SaleService service;
+
 	public SaleController(SaleService service) {
-			this.service = service;
+		this.service = service;
 	}
-	
+
 	@GetMapping
-	public List<Sale> findSales(){
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
 	}
-	
-	
+
 }
--- backend/src/main/java/io/github/wferdinando/dsmeta/services/SaleService.java
+++ backend/src/main/java/io/github/wferdinando/dsmeta/services/SaleService.java
@@ -1,7 +1,11 @@
 package io.github.wferdinando.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import io.github.wferdinando.dsmeta.entities.Sale;
@@ -11,12 +15,18 @@ import io.github.wferdinando.dsmeta.repositories.SaleRepository;
 public class SaleService {
 
 	private final SaleRepository repository;
+
 	public SaleService(SaleRepository repository) {
 		this.repository = repository;
 	}
-	
-	public List<Sale> findSales() {
-		return repository.findAll();
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);// Converte data em string para data
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+		return repository.findSales(min, max, pageable);
 	}
-	
+
 }
